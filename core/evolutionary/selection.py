aa3f9e698a0eab240ab08756e447f7042392cffc
gcarloslima <carlosrelgs@gmail.com>
2025-04-30 19:46:55 -0300

    Correção na lógica de seleção

--- core/evolutionary/selection.py
+++ core/evolutionary/selection.py
@@ -1,14 +1,19 @@
 import random
 
+
 def selecao(populacao_total):
     """
-    Seleciona dois indivíduos aleatórios da população.
-    
+    Seleciona dois indivíduos aleatórios da população,
+    sendo o primeiro escolhido entre a metade melhor
+    (com menor número de choques).
+
     Args:
-        populacao_total (list): A população total
-        
+        populacao_total (list): População total, já ordenada do menor para o maior número de choques.
+
     Returns:
         tuple: Dois indivíduos selecionados
     """
-    escolhido1, escolhido2 = random.sample(populacao_total, 2)
-    return escolhido1, escolhido2 
+    metade_superior = populacao_total[:len(populacao_total) // 2]
+    escolhido1 = random.choice(metade_superior)
+    escolhido2 = random.choice(populacao_total)
+    return escolhido1, escolhido2
